fad64199fd0a83dcf6464dbec629053361d9c863
Tisana Roongsawang <tisana@gmail.com>
2016-10-19 22:13:45 +0700

    initial Spring Boot from Spring Initializr

---
+++ src/main/java/com/dst/hackathon/FpvApiApplication.java
@@ -0,0 +1,12 @@
+package com.dst.hackathon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FpvApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FpvApiApplication.class, args);
+	}
+}
