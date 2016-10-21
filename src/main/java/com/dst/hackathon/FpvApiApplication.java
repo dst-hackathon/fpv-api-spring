d20531fd032704ceeb3d32528202c556355ed533
Tisana Roongsawang <tisana@gmail.com>
2016-10-21 14:40:10 +0700

    Add sample api from data model

--- src/main/java/com/dst/hackathon/FpvApiApplication.java
+++ src/main/java/com/dst/hackathon/FpvApiApplication.java
@@ -1,7 +1,11 @@
 package com.dst.hackathon;
 
+import com.dst.hackathon.fpv.data.model.Employee;
+import com.dst.hackathon.fpv.data.repository.EmployeeRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class FpvApiApplication {
@@ -9,4 +13,17 @@ public class FpvApiApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(FpvApiApplication.class, args);
 	}
+
+	@Bean
+	public CommandLineRunner dataSetup(EmployeeRepository employeeRepository) {
+        return (args) -> {
+            Employee employee = new Employee();
+            employee.setEmployeeId("dt12345");
+            employee.setFirstName("John");
+            employee.setLastName("Doe");
+            employee.setNickName("Done");
+
+            employeeRepository.save(employee);
+        };
+    }
 }
---
+++ src/main/java/com/dst/hackathon/fpv/data/model/Employee.java
@@ -0,0 +1,114 @@
+package com.dst.hackathon.fpv.data.model;
+
+import javax.persistence.*;
+
+/**
+ * Created by Seph on 21/10/2559.
+ */
+
+@Entity
+public class Employee {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    @Column(name = "employee_id", unique = true)
+    private String employeeId;
+
+    private String firstName;
+
+    private String lastName;
+
+    private String nickName;
+
+    private String photoUrl;
+
+    private String businessUnit; //Class
+
+    private String jobPosition;
+
+    private String phoneExtension; //Class
+
+    private String deskCode; //Class
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getEmployeeId() {
+        return employeeId;
+    }
+
+    public void setEmployeeId(String employeeId) {
+        this.employeeId = employeeId;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getNickName() {
+        return nickName;
+    }
+
+    public void setNickName(String nickName) {
+        this.nickName = nickName;
+    }
+
+    public String getPhotoUrl() {
+        return photoUrl;
+    }
+
+    public void setPhotoUrl(String photoUrl) {
+        this.photoUrl = photoUrl;
+    }
+
+    public String getBusinessUnit() {
+        return businessUnit;
+    }
+
+    public void setBusinessUnit(String businessUnit) {
+        this.businessUnit = businessUnit;
+    }
+
+    public String getJobPosition() {
+        return jobPosition;
+    }
+
+    public void setJobPosition(String jobPosition) {
+        this.jobPosition = jobPosition;
+    }
+
+    public String getPhoneExtension() {
+        return phoneExtension;
+    }
+
+    public void setPhoneExtension(String phoneExtension) {
+        this.phoneExtension = phoneExtension;
+    }
+
+    public String getDeskCode() {
+        return deskCode;
+    }
+
+    public void setDeskCode(String deskCode) {
+        this.deskCode = deskCode;
+    }
+}
